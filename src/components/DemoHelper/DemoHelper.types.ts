d38675f6c165e9ce755e2db0565edaa99486168b
Henri Peetsmann <11693189+sidrun@users.noreply.github.com>
2023-11-22 00:59:12 +0200

    Refactored and simplified layouts and pages
    Extended DemoHelper component and added flow guides.
    Small bug fixes & improvements.

--- src/components/DemoHelper/DemoHelper.types.ts
+++ src/components/DemoHelper/DemoHelper.types.ts
@@ -1,10 +1,22 @@
+import { type TProps as TBasicPopoverProps } from "@components/BasicPopover/BasicPopover.types";
+
 export type TProps = {
   attributes?: Record<string, string>;
   button?: TButton;
+  popover?: TPopover;
 };
 
 export type TButton = {
   attributes?: Record<string, string>;
   name: string;
   href: string;
+};
+
+export type TPopover = TBasicPopoverProps & {
+  button?: {
+    text?: string;
+  };
+  panel?: {
+    html?: string;
+  };
 };
---
+++ src/lib/cssClasses.ts
@@ -0,0 +1,2 @@
+export const htmlClass =
+  "text-stone-700 selection:bg-primary-400/40 selection:text-primary-950";

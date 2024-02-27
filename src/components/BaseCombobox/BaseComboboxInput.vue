a48bb1108cbff7bfa6993cf2b5e359caa036feae
Henri Peetsmann <11693189+sidrun@users.noreply.github.com>
2024-02-27 17:29:26 +0200

    Refactored and simplified BaseCombobox component.

--- src/components/BaseCombobox/BaseComboboxInput.vue
+++ src/components/BaseCombobox/BaseComboboxInput.vue
@@ -1,10 +1,9 @@
 <template>
-  <div class="relative">
+  <div class="relative z-10">
     <ComboboxInput
       v-bind="$attrs"
-      @change="query = $event.target.value"
-      @focus="handleFocus"
       :displayValue="(option: any) => option?.value"
+      @change="query = $event.target.value"
       :class="[
         props.appearance == 'white'
           ? 'bg-white'
@@ -23,45 +22,66 @@
       <IconArrowDown1 class="h-3.5 w-3.5" />
     </ComboboxButton>
 
-    <ComboboxOptions
-      v-if="filteredOptions && filteredOptions.length > 0"
-      class="absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm"
+    <TransitionRoot
+      enter="transition ease-out duration-200"
+      enter-from="transform opacity-0 -translate-y-1"
+      enter-to="opacity-100 translate-y-0"
+      leave="transition ease-in duration-75"
+      leave-from="transform opacity-100 translate-y-0"
+      leave-to="opacity-0 -translate-y-1"
+      @after-leave="query = ''"
     >
-      <ComboboxOption
-        v-for="option in filteredOptions"
-        :key="option?.id"
-        :value="option"
-        as="template"
-        v-slot="{ active, selected }"
+      <ComboboxOptions
+        class="absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm"
       >
-        <li
-          :class="[
-            'relative cursor-default select-none py-2 pl-3 pr-10',
-            active ? 'bg-primary-300 text-primary-900' : 'text-stone-800',
-          ]"
+        <div
+          v-if="filteredOptions.length === 0 && query !== ''"
+          class="relative cursor-default select-none px-3 py-2 text-stone-700"
         >
-          <span :class="['block truncate', selected && 'font-semibold']">
-            {{ option?.value }}
-          </span>
-
-          <span
-            v-if="selected"
+          Nothing found.
+        </div>
+        <ComboboxOption
+          v-for="option in filteredOptions"
+          as="template"
+          :key="option?.id"
+          :value="option"
+          v-slot="{ selected, active }"
+        >
+          <li
             :class="[
-              ' absolute inset-y-0 right-0 flex items-center pr-3.5',
-              active ? 'text-primary-900' : 'text-primary-600',
+              'relative cursor-default select-none py-2 pl-3 pr-10',
+              active ? 'bg-primary-300 text-primary-900' : 'text-stone-800',
             ]"
           >
-            <IconCheck1 class="h-3.5 w-3.5" />
-          </span>
-        </li>
-      </ComboboxOption>
-    </ComboboxOptions>
+            <span :class="['block truncate', selected && 'font-semibold']">
+              {{ option?.value }}
+            </span>
+
+            <span
+              v-if="selected"
+              :class="[
+                ' absolute inset-y-0 right-0 flex items-center pr-3.5',
+                active ? 'text-primary-900' : 'text-primary-600',
+              ]"
+            >
+              <IconCheck1 class="h-3.5 w-3.5" />
+            </span>
+          </li>
+        </ComboboxOption>
+      </ComboboxOptions>
+    </TransitionRoot>
   </div>
 </template>
 
 <script setup lang="ts">
 import { inject } from "vue";
-import { ComboboxButton, ComboboxInput, ComboboxOption, ComboboxOptions } from "@headlessui/vue";
+import {
+  ComboboxButton,
+  ComboboxInput,
+  ComboboxOption,
+  ComboboxOptions,
+  TransitionRoot,
+} from "@headlessui/vue";
 import type { Ref, ComputedRef } from "vue";
 import type { TProps } from "@components/BaseCombobox/BaseComboboxInput.types";
 import type { TOption } from "@components/BaseCombobox/BaseCombobox.types";
@@ -75,24 +95,5 @@ defineOptions({
 
 const props = defineProps<TProps>();
 const query = <Ref<string>>inject("query");
-const selectedOption = <Ref<TOption>>inject("selectedOption");
 const filteredOptions = <ComputedRef<TOption[]>>inject("filteredOptions");
-
-/**
- * handleFocus
- *
- * If the query is not empty and there is no selected option, clear the query.
- * If the query is not empty and there is a selected option, but selected option
- * if different than the query, set the query to the selected option.
- */
-
-const handleFocus = () => {
-  if (query && query.value != "") {
-    if (!selectedOption.value?.value) {
-      query.value = "";
-    } else if (selectedOption.value.value != query.value) {
-      query.value = selectedOption.value.value;
-    }
-  }
-};
 </script>

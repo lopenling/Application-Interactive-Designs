85a17d2528ec325fa38494f04127c69b03cb8621
Henri Peetsmann <11693189+sidrun@users.noreply.github.com>
2024-06-18 20:56:37 +0300

    Added initial team edit member modal, various small related improvements.

--- src/components/ModalDialog/ModalDialog.vue
+++ src/components/ModalDialog/ModalDialog.vue
@@ -16,26 +16,8 @@
         class="fixed left-0 top-0 -z-50 h-0 w-0 overflow-hidden opacity-0 outline-none"
       ></button>
 
-      <!-- Backdrop -->
-      <TransitionChild
-        as="template"
-        enter="ease-out duration-300"
-        enter-from="opacity-0"
-        enter-to="opacity-100"
-        leave="ease-in duration-200"
-        leave-from="opacity-100"
-        leave-to="opacity-0"
-      >
-        <div
-          class="fixed inset-0 overflow-hidden bg-transparent transition-opacity sm:bg-stone-950/5"
-        >
-          <div
-            class="absolute left-1/2 top-1/2 h-[100vw] w-[100vw] -translate-x-1/2 -translate-y-1/2 bg-gradient-to-b from-stone-100/50 from-0% to-stone-200 to-90% sm:bg-gradient-radial sm:from-stone-100/70 sm:from-40% sm:to-stone-200 sm:to-100% portrait:h-[100vh] portrait:w-[100vh]"
-          />
-        </div>
-      </TransitionChild>
+      <ModalDialogBackdrop />
 
-      <!-- Modal -->
       <div class="fixed inset-0 z-10 w-screen overflow-y-auto">
         <div class="flex min-h-full items-end justify-center p-4 text-center sm:items-center">
           <TransitionChild
@@ -48,7 +30,7 @@
             leave-to="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
           >
             <DialogPanel
-              class="relative w-full transform overflow-hidden rounded-lg bg-white px-4 pb-4 pt-5 text-left shadow-xl transition-all sm:my-8 sm:p-6"
+              class="relative w-full transform overflow-hidden rounded-lg bg-white p-4 pt-6 text-left shadow-xl transition-all 2xs:p-6 2xs:pt-8 sm:my-8 sm:p-8 sm:pb-6"
               :class="[
                 props.maxWidth == 'xs' && 'sm:max-w-xs',
                 props.maxWidth == 'sm' && 'sm:max-w-sm',
@@ -65,19 +47,19 @@
                 !props.maxWidth && 'sm:max-w-lg',
               ]"
             >
-              <!-- Content -->
               <div class="sm:flex sm:items-start">
+                <!-- Illustration -->
                 <div
                   v-if="slots.illustration"
                   class="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center sm:mx-0 sm:h-10 sm:w-10"
                 >
                   <slot name="illustration" />
                 </div>
+
+                <!-- Content -->
                 <div
-                  :class="[
-                    'text-center sm:text-left',
-                    slots.illustration && 'mt-3 sm:ml-4 sm:mt-0',
-                  ]"
+                  class="grow text-center sm:text-left"
+                  :class="slots.illustration && 'mt-3 sm:ml-4 sm:mt-0'"
                 >
                   <DialogTitle
                     v-if="slots.title"
@@ -87,7 +69,7 @@
                     <slot name="title" />
                   </DialogTitle>
 
-                  <div :class="['text-sm', slots.title && 'mt-2 ']">
+                  <div class="text-sm" :class="slots.title && 'mt-2'">
                     <slot name="default" />
                   </div>
                 </div>
@@ -96,21 +78,65 @@
               <!-- Buttons -->
               <div
                 v-if="slots.buttons"
-                class="mt-6 flex flex-wrap gap-3 sm:mt-4 sm:flex-row-reverse"
+                class="flex flex-wrap gap-x-3 gap-y-4 sm:flex-row-reverse"
+                :class="
+                  props.separateButtons
+                    ? 'mt-4 border-t pt-4 2xs:mt-6 2xs:pt-6 sm:mt-6 sm:pt-3'
+                    : 'mt-6 sm:mt-6'
+                "
               >
                 <slot name="buttons" />
               </div>
 
-              <!-- Close button -->
-              <div v-if="!props.stayOpen" class="absolute right-0 top-0 hidden pr-4 pt-4 sm:block">
-                <button
-                  type="button"
-                  class="-translate-y-0.75 translate-x-0.75 rounded-md bg-white p-1.5 text-stone-400 transition-colors hover:bg-stone-100 hover:text-stone-700 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-primary-600 focus-visible:ring-offset-0"
-                  @click="open = false"
+              <!-- Icon buttons -->
+              <div class="absolute right-0 top-0 pr-4 pt-4 2xs:pr-6 2xs:pt-6 sm:pr-8 sm:pt-8">
+                <div
+                  class="relative -mr-2 -mt-2 flex flex-row gap-1 sm:-mr-2 sm:-mt-2 sm:-translate-y-2.5 sm:translate-x-2.5"
                 >
-                  <span class="sr-only">Close</span>
-                  <IconClose class="h-4 w-4" aria-hidden="true" />
-                </button>
+                  <!-- Options dropdown -->
+                  <Menu v-if="slots.options" as="div" class="block text-left">
+                    <MenuButton
+                      as="button"
+                      class="rounded-md bg-white p-2 text-stone-500 transition-colors hover:bg-stone-100 hover:text-stone-700 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-primary-600 focus-visible:ring-offset-0"
+                    >
+                      <IconNavigationMenuVertical
+                        class="h-5 w-5"
+                        aria-hidden="true"
+                        data-expand-by="2"
+                      />
+                    </MenuButton>
+                    <transition
+                      enter-active-class="transition ease-out duration-100"
+                      enter-from-class="transform opacity-0 scale-95"
+                      enter-to-class="transform opacity-100 scale-100"
+                      leave-active-class="transition ease-in duration-75"
+                      leave-from-class="transform opacity-100 scale-100"
+                      leave-to-class="transform opacity-0 scale-95"
+                    >
+                      <MenuItems
+                        class="absolute right-0 z-10 mt-2 w-48 origin-top-right divide-y divide-stone-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
+                      >
+                        <slot name="options" />
+
+                        <div v-if="!props.stayOpen" class="py-1 sm:hidden">
+                          <ModalDialogOption :close-modal="true">Close dialog</ModalDialogOption>
+                        </div>
+                      </MenuItems>
+                    </transition>
+                  </Menu>
+
+                  <!-- Close icon -->
+                  <button
+                    v-if="!props.stayOpen"
+                    type="button"
+                    class="rounded-md bg-white p-2 text-stone-400 transition-colors hover:bg-stone-100 hover:text-stone-700 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-primary-600 focus-visible:ring-offset-0"
+                    :class="slots.options ? 'hidden sm:flex' : 'flex'"
+                    @click="open = false"
+                  >
+                    <span class="sr-only">Close</span>
+                    <IconClose class="h-5 w-5" aria-hidden="true" data-expand-by="2" />
+                  </button>
+                </div>
               </div>
             </DialogPanel>
           </TransitionChild>
@@ -121,10 +147,15 @@
 </template>
 
 <script setup lang="ts">
+import { Menu, MenuButton, MenuItems } from "@headlessui/vue";
+
 import { ref, provide, useSlots } from "vue";
 import { Dialog, DialogPanel, DialogTitle, TransitionChild, TransitionRoot } from "@headlessui/vue";
 import eventBus from "@scripts/general/eventBus";
+import ModalDialogBackdrop from "@components/ModalDialog/ModalDialogBackdrop.vue";
+import ModalDialogOption from "@components/ModalDialog/ModalDialogOption.vue";
 import IconClose from "@components/icons/streamline/regular/IconClose.vue";
+import IconNavigationMenuVertical from "@components/icons/streamline/regular/IconNavigationMenuVertical.vue";
 
 type TProps = {
   name: string;
@@ -144,6 +175,7 @@ type TProps = {
     | "7xl"
     | "full";
   wideButtons?: boolean;
+  separateButtons?: boolean;
   enableInitialFocus?: boolean;
 };
 
@@ -160,12 +192,14 @@ const handleClose = () => {
 provide("name", props.name);
 provide("wideButtons", props.wideButtons);
 
-eventBus.on("modal-open", (name) => {
-  if (name !== props.name) return;
+eventBus.on("open-modal", (event: any) => {
+  if (event.name !== props.name) return;
+  renderInitialFocusWorkaround.value = true;
   open.value = true;
 });
-eventBus.on("modal-close", (name) => {
-  if (name !== props.name) return;
+
+eventBus.on("close-modal", (event: any) => {
+  if (event.name !== props.name) return;
   open.value = false;
 });
 </script>
---
+++ src/components/icons/streamline/regular/IconNavigationMenuVertical.vue
@@ -0,0 +1,51 @@
+<template>
+  <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" :viewBox="viewBoxValue">
+    <desc>Navigation Menu Vertical Streamline Icon: https://streamlinehq.com</desc>
+    <path
+      d="M9.362 20.628a2.625 2.625 0 1 0 5.25 0 2.625 2.625 0 1 0 -5.25 0Z"
+      fill="none"
+      stroke="currentColor"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      :stroke-width="strokeWidth"
+    ></path>
+    <path
+      d="M9.362 3.378a2.625 2.625 0 1 0 5.25 0 2.625 2.625 0 1 0 -5.25 0Z"
+      fill="none"
+      stroke="currentColor"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      :stroke-width="strokeWidth"
+    ></path>
+    <path
+      d="M9.362 12.003a2.625 2.625 0 1 0 5.25 0 2.625 2.625 0 1 0 -5.25 0Z"
+      fill="none"
+      stroke="currentColor"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      :stroke-width="strokeWidth"
+    ></path>
+  </svg>
+</template>
+
+<script setup lang="ts">
+import { useAttrs } from "vue";
+
+const attrs = useAttrs();
+let viewBoxValue = "0 0 24 24";
+let strokeWidth = "1.5";
+
+if (attrs["data-expand-by"]) {
+  viewBoxValue = viewBoxValue
+    .split(" ")
+    .map((value, index) => {
+      if (index < 2) {
+        return parseInt(value) - Number(attrs["data-expand-by"]);
+      } else {
+        return parseInt(value) + Number(attrs["data-expand-by"]) * 2;
+      }
+    })
+    .join(" ");
+}
+if (attrs["data-stroke-width"]) strokeWidth = attrs["data-stroke-width"].toString();
+</script>

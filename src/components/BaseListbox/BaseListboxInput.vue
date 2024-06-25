62db90f1b05425079365e47a94aa166dad1fc0b4
Henri Peetsmann <henri.peetsmann@gmail.com>
2024-06-25 21:08:51 +0300

    Added BaseListbox component

---
+++ src/components/BaseListbox/BaseListboxInput.vue
@@ -0,0 +1,92 @@
+<template>
+  <div class="relative">
+    <ListboxButton
+      v-bind="$attrs"
+      class="relative w-full cursor-default rounded-md border-0 py-1.5 pl-3 pr-10 text-left text-stone-800 ring-1 ring-inset ring-stone-300 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-primary-600 data-[headlessui-state=open]:outline-none data-[headlessui-state=open]:ring-2 data-[headlessui-state=open]:ring-inset data-[headlessui-state=open]:ring-primary-600 sm:text-sm sm:leading-6"
+      :class="[
+        props.appearance == 'white'
+          ? 'bg-white'
+          : props.appearance == 'transparent'
+            ? 'bg-transparent'
+            : props.appearance == 'gray'
+              ? 'bg-stone-50 focus:bg-primary-50 focus:text-primary-800'
+              : 'bg-stone-50 focus:bg-primary-50 focus:text-primary-800',
+        props.shadow && 'shadow-sm',
+      ]"
+    >
+      <span v-if="selectedOption" v-text="selectedOption?.value" class="block truncate" />
+      <span
+        v-if="!selectedOption"
+        v-text="props.placeholder || 'Select an option'"
+        class="block truncate text-stone-400"
+      />
+      <span
+        class="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-3.5 text-stone-400 transition duration-0 group-[:has(button:focus)]:text-primary-500 group-data-[headlessui-state=open]:text-primary-500"
+      >
+        <IconArrowDown1 class="h-3.5 w-3.5" aria-hidden="true" />
+      </span>
+    </ListboxButton>
+
+    <transition
+      enter-active-class="transition duration-200 ease-out"
+      enter-from-class="transform -translate-y-1 opacity-0"
+      enter-to-class="transform translate-y-0 opacity-100"
+      leave-active-class="transition duration-75 ease-in"
+      leave-from-class="transform translate-y-0 opacity-100"
+      leave-to-class="transform -translate-y-1 opacity-0"
+    >
+      <ListboxOptions
+        class="absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm"
+      >
+        <ListboxOption
+          v-for="option in sortedOptions"
+          as="template"
+          :key="option?.id"
+          :value="option"
+          v-slot="{ active, selected }"
+        >
+          <li
+            class="relative cursor-default select-none py-2 pl-3 pr-10"
+            :class="[active ? 'bg-primary-300 text-primary-900' : 'text-stone-800']"
+          >
+            <span class="block truncate" :class="[selected ? 'font-semibold' : 'font-normal']">
+              {{ option?.value }}
+            </span>
+
+            <span
+              v-if="selected"
+              class="absolute inset-y-0 right-0 flex items-center pr-3.5"
+              :class="[active ? 'text-primary-900' : 'text-primary-600']"
+            >
+              <IconCheck1 class="h-3.5 w-3.5" aria-hidden="true" />
+            </span>
+          </li>
+        </ListboxOption>
+      </ListboxOptions>
+    </transition>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { inject } from "vue";
+import { ListboxButton, ListboxOption, ListboxOptions } from "@headlessui/vue";
+import type { ComputedRef } from "vue";
+import { type TOption } from "@components/BaseListbox/BaseListbox.vue";
+
+defineOptions({
+  inheritAttrs: false,
+});
+
+type TProps = {
+  appearance?: "gray" | "white" | "transparent";
+  shadow?: boolean;
+  placeholder?: string;
+};
+
+const props = defineProps<TProps>();
+const selectedOption = <ComputedRef<TOption>>inject("selectedOption");
+const sortedOptions = <ComputedRef<TOption[]>>inject("sortedOptions");
+
+import IconArrowDown1 from "@components/icons/streamline/regular/IconArrowDown1.vue";
+import IconCheck1 from "@components/icons/streamline/regular/IconCheck1.vue";
+</script>
---
+++ src/components/BaseListbox/BaseListboxLabel.vue
@@ -0,0 +1,9 @@
+<template>
+  <ListboxLabel class="mb-2 block text-sm font-medium leading-6 text-stone-900">
+    <slot name="default" />
+  </ListboxLabel>
+</template>
+
+<script setup lang="ts">
+import { ListboxLabel } from "@headlessui/vue";
+</script>

afe574b3584a5536f916beb97e1937caef0b7a22
Henri Peetsmann <henri.peetsmann@gmail.com>
2024-06-26 11:23:45 +0300

    Simplified and fixed team edit member modal

--- src/pages/teams/[id]/_partials/_SingularTeamModalEditMember.vue
+++ src/pages/teams/[id]/_partials/_SingularTeamModalEditMember.vue
@@ -1,23 +1,28 @@
 <template>
-  <ModalDialog v-if="hasData" name="team-edit-member" max-width="xl" :separate-buttons="true">
+  <ModalDialog
+    v-if="singularUser && selectedRoleInEditTeamMember"
+    name="team-edit-member"
+    max-width="xl"
+    :separate-buttons="true"
+  >
     <div class="mb-4 flex flex-col">
       <div class="text-base font-semibold leading-6 text-stone-900">
-        {{ getUserFullNameById(data.user.id) }}
+        {{ usersStore.getUserFullNameById(singularUser.id) }}
       </div>
       <div class="truncate text-xs leading-5 text-stone-500/85">
-        {{ data.user.email }}
+        {{ singularUser.email }}
       </div>
     </div>
 
     <div class="mb-0 w-full text-left sm:w-1/2">
-      <BaseListbox v-model="selectedRoleInEditTeamMember" :options="userRoles">
+      <BaseListbox v-model="selectedRoleInEditTeamMember" :options="sortedUserRolesArray">
         <BaseListboxLabel>Role</BaseListboxLabel>
         <BaseListboxInput appearance="white" />
       </BaseListbox>
     </div>
 
     <template #illustration>
-      <BaseAvatar :user="data.user" size-class="size-full" />
+      <BaseAvatar :user="singularUser" size-class="size-full" />
     </template>
     <template #buttons>
       <ModalDialogButton appearance="primary" :close-modal="true">
@@ -33,17 +38,15 @@
 </template>
 
 <script setup lang="ts">
-import { ref, watch, computed } from "vue";
-
+import { ref } from "vue";
 import eventBus from "@scripts/general/eventBus";
-import usersData, { type TUser } from "@scripts/data/usersData";
-import getUserRoleInTeamById from "@scripts/helpers/getUserRoleInTeamById";
-import getUserFullNameById from "@scripts/helpers/getUserFullNameById";
+import sortArrayByKey from "@scripts/helpers/sortArrayByKey";
+import { useTeamsStore } from "@stores/teamsStore";
+import { useUsersStore, userRoles, type TUser, type TSingularUserRole } from "@stores/usersStore";
 
 import ModalDialog from "@components/ModalDialog/ModalDialog.vue";
 import ModalDialogButton from "@components/ModalDialog/ModalDialogButton.vue";
 import ModalDialogOption from "@components/ModalDialog/ModalDialogOption.vue";
-
 import BaseAvatar from "@components/BaseAvatar/BaseAvatar.vue";
 import BaseListbox from "@components/BaseListbox/BaseListbox.vue";
 import BaseListboxLabel from "@components/BaseListbox/BaseListboxLabel.vue";
@@ -54,30 +57,19 @@ export type TSingularTeamModalEditMember = {
   teamId: number;
 };
 
-type TData = {
-  user: TUser;
-  teamId: number;
-};
-
-const users = usersData();
-const data = ref({} as TData);
-const hasData = ref(true);
-const selectedRoleInEditTeamMember = ref(null);
+const teamsStore = useTeamsStore();
+const usersStore = useUsersStore();
+const singularUser = ref({} as TUser);
+const selectedRoleInEditTeamMember = ref({} as TSingularUserRole);
+const sortedUserRolesArray = sortArrayByKey(Object.values(userRoles), "label");
 
 eventBus.on("open-modal", (event: any) => {
   if (event.name !== "team-edit-member") return;
-  const singularUser = users.find((user) => user.id === event.data.userId);
 
-  if (!singularUser || !event.data.teamId) {
-    hasData.value = false;
-    return;
-  }
-  data.value.user = singularUser;
-  data.value.teamId = event.data.teamId;
+  selectedRoleInEditTeamMember.value = teamsStore.getUserRoleByUserId(
+    event.data.userId,
+    event.data.teamId,
+  )!;
+  singularUser.value = usersStore.getUserById(event.data.userId)!;
 });
-
-const userRoles = [
-  { id: 1, value: "Administrator", selected: true },
-  { id: 2, value: "Member" },
-];
 </script>

7036056ebb7e72e4be415a8f499b562b6653b3cc
Henri Peetsmann <henri.peetsmann@gmail.com>
2024-06-27 23:35:55 +0300

    Added initial team new custom dictionary modal

---
+++ src/pages/teams/[id]/_partials/_SingularTeamModalNewCustomDictionary.vue
@@ -0,0 +1,68 @@
+<template>
+  <ModalDialog name="team-new-custom-dictionary" max-width="2xl" :separate-buttons="true">
+    <p class="max-w-prose text-pretty text-left">
+      The CSV file must be in the correct format, make sure it follows the template which you can
+      <button
+        type="button"
+        class="-mx-px inline-flex rounded-md px-px font-semibold text-primary-600 underline underline-offset-2 transition hover:text-primary-500 focus-visible:no-underline focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-primary-600 focus-visible:ring-offset-0"
+      >
+        download here</button
+      >.
+    </p>
+    <div class="mb-6 mt-6 flex flex-col gap-x-4 gap-y-2.5 text-left sm:mb-4">
+      <div class="sm:w-1/2">
+        <BaseLabel for="dictionary-name">Name</BaseLabel>
+        <BaseInput
+          appearance="white"
+          type="text"
+          name="dictionary-name"
+          id="dictionary-name"
+          inputmode="text"
+          required
+        />
+      </div>
+      <div>
+        <BaseLabel for="dictionary-file">CSV file</BaseLabel>
+        <BaseInput
+          appearance="white"
+          type="text"
+          name="dictionary-file"
+          id="dictionary-file"
+          inputmode="text"
+          required
+        />
+      </div>
+    </div>
+    <p class="max-w-prose text-left text-stone-500">
+      The uploaded CSV file size must be less than 100MB.<br />
+      A team can have up to {{ MAX_CUSTOM_DICTIONARIES }} custom dictionaries.
+    </p>
+
+    <template #title>Add a new custom dictionary</template>
+    <template #illustration>
+      <ModalDialogIllustration :icon-component="IconBookEdit" appearance="primary" />
+    </template>
+    <template #buttons>
+      <ModalDialogButton appearance="primary" :close-modal="true">
+        Upload dictionary
+      </ModalDialogButton>
+    </template>
+  </ModalDialog>
+</template>
+
+<script setup lang="ts">
+import eventBus from "@scripts/general/eventBus";
+import { MAX_CUSTOM_DICTIONARIES } from "@scripts/data/constants";
+
+import ModalDialog from "@components/ModalDialog/ModalDialog.vue";
+import ModalDialogButton from "@components/ModalDialog/ModalDialogButton.vue";
+import ModalDialogIllustration from "@components/ModalDialog/ModalDialogIllustration.vue";
+import BaseLabel from "@components/BaseLabel/BaseLabel.vue";
+import BaseInput from "@components/BaseInput/BaseInput.vue";
+
+import IconBookEdit from "@components/icons/streamline/regular/IconBookEdit.vue";
+
+eventBus.on("open-modal", (event: any) => {
+  if (event.name !== "team-new-custom-dictionary") return;
+});
+</script>

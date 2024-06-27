f2fc7a6fa7c38e7835778e4c284b9b6709d647ef
Henri Peetsmann <11693189+sidrun@users.noreply.github.com>
2024-06-27 19:09:42 +0300

    Improved ModalDialog design and usage versatility

--- src/components/ModalDialog/ModalDialogBackdrop.vue
+++ src/components/ModalDialog/ModalDialogBackdrop.vue
@@ -10,7 +10,7 @@
   >
     <div class="fixed inset-0 overflow-hidden bg-transparent transition-opacity sm:bg-stone-950/5">
       <div
-        class="absolute left-1/2 top-1/2 h-[100vw] w-[100vw] -translate-x-1/2 -translate-y-1/2 bg-gradient-to-b from-stone-100/60 from-0% to-stone-200 to-90% sm:bg-gradient-radial sm:from-stone-100/70 sm:from-40% sm:to-stone-200 sm:to-100% portrait:h-[100vh] portrait:w-[100vh]"
+        class="absolute left-1/2 top-1/2 h-[100vw] w-[100vw] -translate-x-1/2 -translate-y-1/2 bg-gradient-to-b from-stone-100/60 from-0% to-stone-200 to-90% sm:bg-gradient-radial sm:from-stone-100/75 sm:from-40% sm:to-stone-200 sm:to-100% portrait:h-[100vh] portrait:w-[100vh]"
       />
     </div>
   </TransitionChild>

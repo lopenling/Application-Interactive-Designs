e243ce3e8b0d073d97c318c921bb2f1ef96bcb86
Henri Peetsmann <henri.peetsmann@gmail.com>
2024-06-28 23:58:01 +0300

    Updated teams new team modal

--- src/pages/teams/_partials/_TeamsModalNewTeam.vue
+++ src/pages/teams/_partials/_TeamsModalNewTeam.vue
@@ -1,12 +1,13 @@
 <template>
   <ModalDialog name="teams-new-team" max-width="md">
     <div class="mt-4 flex flex-col gap-x-4 gap-y-2.5 text-left">
-      <form id="newTeamForm" action="/teams/1">
+      <form id="newTeamForm" @submit.prevent="handleFormSubmit">
         <BaseInputText
+          v-model="newTeamName"
           appearance="white"
           type="text"
-          name="teamName"
-          id="teamName"
+          name="newTeamName"
+          id="newTeamName"
           inputmode="text"
           pattern="[a-zA-Z0-9_\-\(\)][a-zA-Z0-9_\-\(\) ]{1,49}"
           autocomplete="off"
@@ -26,11 +27,36 @@
 </template>
 
 <script setup lang="ts">
+import { ref } from "vue";
 import eventBus from "@scripts/general/eventBus";
+import { useTeamsStore } from "@stores/teamsStore";
+
+import { type AstroGlobal } from "astro";
+import getRole from "@scripts/helpers/getRole";
+import getUrlParams from "@scripts/helpers/getUrlParams";
+import signedInUserData from "@scripts/data/signedInUserData";
 
 import ModalDialog from "@components/ModalDialog/ModalDialog.vue";
 import ModalDialogButton from "@components/ModalDialog/ModalDialogButton.vue";
 import BaseInputText from "@components/BaseInputText/BaseInputText.vue";
+
+type TProps = { astro: AstroGlobal };
+const props = defineProps<TProps>();
+const role = getRole(props.astro);
+const signedInUser = signedInUserData(role);
+
+const teamsStore = useTeamsStore();
+const newTeamName = ref("");
+const newTeamId = ref(teamsStore.getNewTeamId());
+
+const handleFormSubmit = () => {
+  teamsStore.addTeam({
+    teamId: newTeamId.value,
+    teamName: newTeamName.value,
+    userId: signedInUser.id,
+  });
+  window.location.href = `/teams/${newTeamId.value}?` + getUrlParams(props.astro);
+};
 
 eventBus.on("open-modal", (event: any) => {
   if (event.name !== "teams-new-team") return;

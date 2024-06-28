52773b23999b4cee3c7b51d05cb1c5f2ed7553ec
Henri Peetsmann <henri.peetsmann@gmail.com>
2024-06-28 16:39:45 +0300

    Included tailwindcss-color-mix plugin

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,10 @@ const svgToDataUri = require("mini-svg-data-uri");
 const defaultTheme = require("tailwindcss/defaultTheme");
 const plugin = require("tailwindcss/plugin");
 
+const { default: toColorValue } = require("tailwindcss/lib/util/toColorValue");
+const { default: flattenColorPalette } = require("tailwindcss/lib/util/flattenColorPalette");
+const { default: withAlphaVariable } = require("tailwindcss/lib/util/withAlphaVariable");
+
 const customColors = {
   primary: {
     50: "#F5FAF5",
@@ -163,6 +167,89 @@ module.exports = {
           "--tw-ring-color": theme("colors.primary.600"),
         },
       });
+    }),
+
+    // tailwindcss-color-mix plugin by Javier Morales (JavierM42)
+    plugin(({ matchUtilities, theme, corePlugins }) => {
+      const { bgMix, bgMixAmount, bgMixMethod } = {
+        bgMix: "bg-mix",
+        bgMixAmount: "bg-mix-amount",
+        bgMixMethod: "bg-mix-method",
+      };
+      // add bg-mix utility
+      matchUtilities(
+        {
+          [bgMix]: (value) => {
+            const { mixColor, ...rest } = withAlphaVariable({
+              color: value,
+              property: "mixColor",
+              variable: "--tw-bg-mix-opacity",
+            });
+
+            return {
+              ...rest,
+              "background-color": `color-mix(var(--tw-bg-mix-method, in srgb), ${mixColor} calc(var(--tw-bg-mix-amount, 0) * 1%), var(--tw-bg-base))`,
+            };
+          },
+        },
+        {
+          values: flattenColorPalette(theme("backgroundColor")),
+          type: ["color", "any"],
+        },
+      );
+      // add bg-mix-amount utility
+      matchUtilities(
+        {
+          [bgMixAmount]: (value) => ({
+            "--tw-bg-mix-amount": value,
+          }),
+        },
+        {
+          values: Object.fromEntries(
+            Object.entries(theme("backgroundOpacity")).map(([key, value]) => [
+              key,
+              `${value * 100}`,
+            ]),
+          ),
+        },
+      );
+      // add bg-mix-space utility
+      matchUtilities(
+        {
+          [bgMixMethod]: (value) => ({
+            "--tw-bg-mix-method": value,
+          }),
+        },
+        {
+          values: {
+            srgb: "in srgb",
+            "shorter-hue": "in hsl shorter hue",
+            "longer-hue": "in hsl longer hue",
+          },
+        },
+      );
+      // add --tw-bg-base to bg-utility
+      matchUtilities(
+        {
+          bg: (value) => {
+            if (!corePlugins("backgroundOpacity")) {
+              return {
+                "background-color": toColorValue(value),
+              };
+            }
+
+            return withAlphaVariable({
+              color: value,
+              property: "--tw-bg-base",
+              variable: "--tw-bg-opacity",
+            });
+          },
+        },
+        {
+          values: flattenColorPalette(theme("backgroundColor")),
+          type: ["color", "any"],
+        },
+      );
     }),
   ],
 };

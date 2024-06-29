9184e163fce1b36b5aaf94f0b808574983702a41
Henri Peetsmann <henri.peetsmann@gmail.com>
2024-06-30 01:26:48 +0300

    Added teams leave team modal and simplified store usage

---
+++ src/components/icons/streamline/regular/IconRoadSignWarning.vue
@@ -0,0 +1,51 @@
+<template>
+  <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" :viewBox="viewBoxValue">
+    <desc>Road Sign Warning Streamline Icon: https://streamlinehq.com</desc>
+    <path
+      stroke="currentColor"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      d="M13.313 2.27521c-0.1297 -0.2347 -0.3199 -0.43035 -0.5508 -0.5666 -0.2309 -0.13626 -0.4941 -0.20812 -0.7622 -0.20812 -0.2682 0 -0.5314 0.07186 -0.7623 0.20812 -0.2309 0.13625 -0.4211 0.3319 -0.5507 0.5666L0.936968 20.2752c-0.126082 0.2284 -0.19043 0.4857 -0.186692 0.7465 0.003738 0.2608 0.075432 0.5162 0.208006 0.7408 0.132578 0.2247 0.321438 0.4109 0.547968 0.5403 0.22652 0.1293 0.48286 0.1974 0.74372 0.1974H21.75c0.2608 0 0.5172 -0.0681 0.7437 -0.1974 0.2265 -0.1294 0.4154 -0.3156 0.548 -0.5403 0.1325 -0.2246 0.2042 -0.48 0.208 -0.7408 0.0037 -0.2608 -0.0607 -0.5181 -0.1867 -0.7465l-9.75 -17.99999Z"
+      :stroke-width="strokeWidth"
+    ></path>
+    <path
+      stroke="currentColor"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      d="M12 15V8.25"
+      :stroke-width="strokeWidth"
+    ></path>
+    <path
+      stroke="currentColor"
+      d="M12 18.75c-0.2071 0 -0.375 -0.1679 -0.375 -0.375S11.7929 18 12 18"
+      :stroke-width="strokeWidth"
+    ></path>
+    <path
+      stroke="currentColor"
+      d="M12 18.75c0.2071 0 0.375 -0.1679 0.375 -0.375S12.2071 18 12 18"
+      :stroke-width="strokeWidth"
+    ></path>
+  </svg>
+</template>
+
+<script setup lang="ts">
+import { useAttrs } from "vue";
+
+const attrs = useAttrs();
+let viewBoxValue = "0 0 24 24";
+let strokeWidth = "1.5";
+
+if (attrs["data-expand-by"]) {
+  viewBoxValue = viewBoxValue
+    .split(" ")
+    .map((value, index) => {
+      if (index < 2) {
+        return parseInt(value) - Number(attrs["data-expand-by"]);
+      } else {
+        return parseInt(value) + Number(attrs["data-expand-by"]) * 2;
+      }
+    })
+    .join(" ");
+}
+if (attrs["data-stroke-width"]) strokeWidth = attrs["data-stroke-width"].toString();
+</script>

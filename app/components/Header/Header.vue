7e371201a0c27b280c75d3a6affb4732fa6817de
Henri Peetsmann <11693189+sidrun@users.noreply.github.com>
2024-12-02 14:58:35 +0200

    Added header height variable

--- app/components/Header/Header.vue
+++ app/components/Header/Header.vue
@@ -1,6 +1,6 @@
 <template>
   <header class="bg-white shadow-sm">
-    <div class="container-xl flex min-h-16 gap-x-9">
+    <div class="container-xl flex h-[--header-height] gap-x-9">
       <div class="flex grow gap-x-9">
         <div class="flex items-center">
           <NuxtLink to="/">
